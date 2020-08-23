7b8a4f4ceba9721c1640aafc3097d55061604102
ngecu <devngecu@gmail.com>
2020-08-23 20:30:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+    state = {
+        activity:" ",
+        type:" ",
+        participants : " "
+    }
 
-export default App;
+    componentDidMount(){
+        this.fetchActivity();
+    }
+    fetchActivity = () =>{
+        axios.get("https://www.boredapi.com/api/activity")
+        .then((response) =>{
+            const {activity,type,participants} = response.data 
+          
+            this.setState({activity,type,participants})
+        })
+        .catch((error)=>{
+            console.log(error)
+        });
+    }
+    render() {
+        return (
+            <div className="app">
+                <div className="card">
+                    <h1 className="activity">Activity:
+                     <span className="heading">
+                    {this.state.activity}
+                        </span></h1>
+                    <h1 className="type">Activity Type : <br/>
+                    <span className="typeHeading">
+                    {this.state.type}
+                    </span>
+                    </h1> 
+                    <h1 className="participants">Participants: <br/>
+                    <span className="participantsHeading">
+                    {this.state.participants}
+                    </span>
+                     
+                     </h1> 
+                    <button className="button" onClick={this.fetchActivity}>
+                        <span>FUN ACTIVITY</span>
+                    </button>
+                </div>
+               
+            </div>
+
+        )
+    }
+}
